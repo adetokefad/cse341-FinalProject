d06320d4b00bde30e23ee9d0dd835c8c7c5a7e1b
adetokefad <adetokefaderin@gmail.com>
2026-08-14 18:20:51 +0100

    Fix Swagger documentation and tests

--- swagger.js
+++ swagger.js
@@ -4,12 +4,14 @@ const doc = {
   info: {
     title: "MovieVault API",
     description: "A movie tracking and review REST API",
+    version: "1.0.0",
   },
   host: "cse341-finalproject-gbg5.onrender.com",
   schemes: ["https"],
 };
 
 const outputFile = "./swagger-output.json";
-const routes = ["./server.js"];
+
+const routes = ["./app.js"];
 
 swaggerAutogen(outputFile, routes, doc);
